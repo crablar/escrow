299fb41b01e2d114944f4ca7bd1ba236f9426250
Jeffrey Meyerson <jmeyerson@jeff.(none)>
2014-04-17 11:48:00 -0700

    added bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true,
             length: { minimum: 5 }
+  validates :min_bet, presence: true,
+            :numericality => {:only_integer => true}
 end
