f97f8e39983a20b4fe8c7d935bf569bdecff7e48
Jeffrey Meyerson <jmeyerson@jeff.(none)>
2014-04-29 10:40:18 -0700

    added some ajax behavior

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Escrow::Application.routes.draw do
   devise_for :users
   #get 'articles/new'
 
+  post 'articles/check_expiration_all'
+
   resources :articles do
     put :bet_yes, :on => :collection
     put :bet_no, :on => :collection
