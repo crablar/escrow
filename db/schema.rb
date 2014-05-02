24bc269ed2dee2a1553cff995c8fdc9b738e66f2
Jeffrey Meyerson <jmeyerson@jeff.(none)>
2014-05-02 12:49:39 -0700

    added avatar selector, doesn't work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501231959) do
+ActiveRecord::Schema.define(version: 20140502192210) do
 
   create_table "article_to_followers", force: true do |t|
     t.integer  "article_id"
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20140501231959) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "",                      null: false
+    t.string   "encrypted_password",     default: "",                      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,                       null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140501231959) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "balance",                default: 100.0
+    t.string   "avatar",                 default: "/assets/anonymous.jpg"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
