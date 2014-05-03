894062fdfd9aafc246fe14402ec9da9252caf376
Jeffrey Meyerson <meff.jeyerson@gmail.com>
2014-05-03 11:33:47 -0700

    updated gem file (again)

--- db/migrate/20140418235758_add_total_bets_to_articles.rb
+++ db/migrate/20140418235758_add_total_bets_to_articles.rb
@@ -1,6 +1,5 @@
 class AddTotalBetsToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :total_bets, :float
-    t.float :total_bets, default: 0
+    add_column :articles, :total_bets, :default => 0
   end
 end
--- db/migrate/20140419175104_add_yes_and_no_bet_amounts_to_articles.rb
+++ db/migrate/20140419175104_add_yes_and_no_bet_amounts_to_articles.rb
@@ -1,6 +1,5 @@
 class AddYesAndNoBetAmountsToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :yes_bet_total, :float
-    t.float :yes_bet_total, default: 0
+    add_column :articles, :yes_bet_total, :float, :default => 0
   end
 end
